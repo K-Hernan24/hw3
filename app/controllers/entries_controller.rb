52124c28d43bef4c3ee09c5c32d4932e23fa1626
Hernan Mendoza <hernan.mendoza@kellogg.northwestern.edu>
2025-05-15 07:10:36 +0000

    Entries Logic

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -17,7 +17,8 @@ class EntriesController < ApplicationController
   # Assign user-entered form data to Entry table in DB
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occured_on"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
